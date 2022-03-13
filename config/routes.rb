5669fa4beaca46c2c43e2650654204e15e8a6f8b
alparslanustaa <alparslanustaa@gmail.com>
2022-03-13 18:28:28 +0000

    user-auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
   resources :about, only: [:index]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   
   resource :cart, only: [:show] do
     post   :add_item
